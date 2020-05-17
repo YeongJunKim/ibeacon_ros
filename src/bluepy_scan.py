36bb8a89dde1346972612f0a341326330d8175eb
colson <dud3722000@naver.com>
2020-05-17 10:07:20 +0900

    add pub

--- src/bluepy_scan.py
+++ src/bluepy_scan.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from bluepy.btle import Scanner, DefaultDelegate
 from ibeacon_ros.msg import beacon_frame
+import rospy
+import array
+import struct
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -13,24 +16,45 @@ class ScanDelegate(DefaultDelegate):
         # elif isNewData:
         #     print "Received new data from", dev.addr
 
-while(1):
-    scanner = Scanner().withDelegate(ScanDelegate())
-    devices = scanner.scan(0.05)
-    for dev in devices:
-        #print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-        for (adtype, desc, value) in dev.getScanData():
-            if desc == "Manufacturer":
-                if value[8:14] == "74278b":
-                    print value
-                    print dev.addr
-                    print dev.rssi
 
-        #print(dev.getScanData())
-        
-        
-        # for (adtype, desc, value) in dev.getScanData():
-        #     print "  %s = %s" % (desc, value)
-        #     if desc == "Manufacturer":
-        #         print value[8:14]
+if __name__ == '__main__':
+    pub = rospy.Publisher('/beacon/info', beacon_frame, queue_size=1)
+    rospy.init_node('bluepy', anonymous=True)
+    while(1):
+        scanner = Scanner().withDelegate(ScanDelegate())
+        devices = scanner.scan(0.05)
+        for dev in devices:
+            #print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+            for (adtype, desc, value) in dev.getScanData():
+                if desc == "Manufacturer":
+                    value = value.encode('utf-8')
+                    if value[8:16] == "74278bda":
+                        data = beacon_frame()
+                        data.factory_id = value[0:8]
+                        data.ibeacon_id = value[8:40]
+                        data.major = value[40:44]
+                        data.minor = value[44:48]
+                        data.RSSI = dev.rssi
+                        num = value[48:50]
+                        num = num.encode()
+                        print type(num)
+                        print num
+                        # num = bytearray(value[48:50])                        
+                        print "data tx power : ", value[48:50]
+                        
+                        # data.max
+                        # data.RSSI
+                        # data.txpower
+                        print data
+                        # print value
+                        # print dev.addr
+                        # print dev.rssi
+                        pub.publish(data)
 
-        
+            #print(dev.getScanData())
+            
+            
+            # for (adtype, desc, value) in dev.getScanData():
+            #     print "  %s = %s" % (desc, value)
+            #     if desc == "Manufacturer":
+            #         print value[8:14]
